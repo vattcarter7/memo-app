7f79a52038f6cf29ddf6245516e13f769e55ff4a
Vatt Sopheak <vattcarter@gmail.com>
2024-03-23 17:00:54 +0700

    style: changed text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,7 @@ export default function Home() {
     <div className="bg-gradient-to-r min-h-screen grainy from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <h1 className="font-semibold text-7xl text-center">
-          My <span className="text-purple-600 font-bold">Memo</span>{' '}
-          Store.
+          My <span className="text-purple-600 font-bold">Memo</span> Diary.
         </h1>
         <div className="mt-4"></div>
         <div className="mt-8"></div>
